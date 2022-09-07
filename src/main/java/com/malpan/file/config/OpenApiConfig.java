bf98fcd6935ef8d279ef89e1235795fc6a37baa3
malossov <lshy1104@163.com>
2022-09-07 21:24:55 +0800

    Normal Fixing bugs

--- src/main/java/com/malpan/file/config/OpenApiConfig.java
+++ src/main/java/com/malpan/file/config/OpenApiConfig.java
@@ -16,18 +16,6 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class OpenApiConfig {
-//     @Bean
-//     public OpenAPI qiwenFileOpenAPI() {
-//         return new OpenAPI()
-//                 .info(new Info().title("网盘项目 API")
-//                         .description("基于springboot + vue 框架开发的Web文件系统，旨在为用户提供一个简单、方便的文件存储方案，能够以完善的目录结构体系，对文件进行管理 。")
-//                         .version("v1.0.0")
-//                         .license(new License().name("MIT").url("http://springdoc.org")))
-//                 .externalDocs(new ExternalDocumentation()
-//                         .description("网盘gitee地址")
-//                         .url("https://www.gitee.com/qiwen-cloud/qiwen-file"));
-//     }
-
     @Bean(value = "indexApi")
     public Docket indexApi() {
             return new Docket(DocumentationType.OAS_30)
